52a28c0af4fcd1983fa1574879ccd097d2401649
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-02-10 14:39:56 -0800

    Add priority incrementation to tasks

--- src/index.js
+++ src/index.js
@@ -3,11 +3,5 @@ import { projectFactory } from "./modules/project";
 const task = todoItemFactory(
   "Complete ToDo list app",
   "Build a simple todo list application in JS",
-  Date(),
-  1
+  Date()
 );
-
-let inbox = projectFactory("Inbox");
-inbox.storage.push(task);
-inbox.storage[0].setCompletion();
-console.log(inbox.storage[0].getCompletion());
--- src/modules/todo.js
+++ src/modules/todo.js
@@ -1,12 +1,29 @@
-const todoItemFactory = (title, description, dueDate, priority) => {
+const todoItemFactory = (title, description, dueDate) => {
   let completion = false;
+  let priority = 1;
   const getCompletion = () => {
     return completion;
   };
   const setCompletion = () => {
     completion = true;
   };
-
+  const getPriority = () => {
+    return priority;
+  };
+  const incrementPriority = () => {
+    if (priority >= 3) {
+      return;
+    } else {
+      priority += 1;
+    }
+  };
+  const decrementPriority = () => {
+    if (priority <= 1) {
+      return;
+    } else {
+      priority -= 1;
+    }
+  };
   return {
     title,
     description,
@@ -14,6 +31,9 @@ const todoItemFactory = (title, description, dueDate, priority) => {
     priority,
     getCompletion,
     setCompletion,
+    getPriority,
+    incrementPriority,
+    decrementPriority,
   };
 };
 
