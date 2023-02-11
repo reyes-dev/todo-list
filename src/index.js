940e2de6f4b15d4c4357b926b305c2901f31c6cc
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-02-11 14:29:03 -0800

    Display task data

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import { todoItemFactory } from "./modules/todo";
 import { projectFactory } from "./modules/project";
+import { setupTodoElements } from "./modules/display";
+const element = document.querySelector("div#content");
+
 const task = todoItemFactory(
   "Complete ToDo list app",
   "Build a simple todo list application in JS",
   Date()
 );
+
+const taskInfo = setupTodoElements(task);
+element.appendChild(taskInfo.title);
+element.appendChild(taskInfo.description);
+element.appendChild(taskInfo.dueDate);
--- src/modules/display.js
+++ src/modules/display.js
@@ -1,5 +1,12 @@
-const element = document.querySelector("div#content");
-
-export default function display(element) {
+const setupTodoElements = (todo) => {
   const title = document.createElement("h1");
-}
+  const description = document.createElement("p");
+  const dueDate = document.createElement("p");
+
+  title.innerHTML = todo.title;
+  description.innerHTML = todo.description;
+  dueDate.innerHTML = todo.dueDate;
+  return { title, description, dueDate };
+};
+
+export { setupTodoElements };
